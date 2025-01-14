66db66c6991c1c2d48f1f5aebae700b489c6b767
Adwait Kadam <adwaitvk22@iitk.ac.in>
2025-01-14 18:53:34 +0530

    Prepared Backend for deployment

--- Backend/index.js
+++ Backend/index.js
@@ -31,6 +31,8 @@ app.use((req, res) => {
   res.status(404).json({ error: "Not Found" });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
+
+module.exports = app;
